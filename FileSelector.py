34de022679321905d643219cec80d7a32d7cd162
Achiwilms <philip901203777@gmail.com>
2023-10-20 15:31:45 +0800

    complete fileselector

--- FileSelector.py
+++ FileSelector.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QFileDialog, QPushButton, QApplication
+from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QFileDialog, QPushButton, QApplication
+from LanguageSelector import LanguageSelector
 
 class FileSelector(QWidget):
     def __init__(self):
@@ -10,14 +11,22 @@ class FileSelector(QWidget):
         # Set the window size
         self.setGeometry(100, 100, 1000, 600)
 
-        # Create a button to select a file and Start OCR
+        # Create a button to select file
+        self.word_label = QLabel('Please select one or more PDF files for OCR', self)
         self.file_button = QPushButton('Select File', self)
 
         # Create a layout to add checkboxes and the buttons
         layout = QVBoxLayout()
 
         # Add the button to the layout
+        layout.addWidget(self.word_label)
         layout.addWidget(self.file_button)
+ 
+        # Change the font type
+        font = self.word_label.font()
+        font.setFamily("Arial")  # Replace with your desired font family
+        font.setPointSize(12)    # Set font size      
+        self.word_label.setFont(font)
 
         # Set the layout for the main window
         self.setLayout(layout)
@@ -32,6 +41,19 @@ class FileSelector(QWidget):
         for file_path in all_file_paths:
             if file_path.endswith(".pdf"):
                 self.file_paths.append(file_path)
+                # print(f"Selected file: {file_path.split('/')[-1]}")
+
+        # check if at least one file is selected
+        if len(self.file_paths) == 0:
+            self.word_label.setText("[ERROR] Please select at least one file.")
+            return
+
+        # close this window
+        self.close()
+
+        # Open the language selector window and pass a reference to self
+        self.progress_window = LanguageSelector(self)
+        self.progress_window.show()
         return 
 
 if __name__ == '__main__':
