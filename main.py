cf822fe97f531f60e14b0550bfec326ea239a6a0
Achiwilms <philip901203777@gmail.com>
2023-10-20 15:32:03 +0800

    snapshot

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from ocr_app import ocr_app
+import sys
+from PyQt5.QtWidgets import QApplication
+from FileSelector import FileSelector
 
 def main():
-	app = ocr_app()
-	app.mainloop()
+    app = QApplication(sys.argv)
+    window = FileSelector()
+    window.show()
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
     main()
--- playground.py
+++ playground.py
@@ -1,5 +1,4 @@
 import sys
-import time
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel
 
 class OCRProgressWindow(QWidget):
@@ -12,13 +11,22 @@ class OCRProgressWindow(QWidget):
         self.close_button = QPushButton('Close', self)
         self.close_button.clicked.connect(self.close_window)
 
+        self.dynamic_button = QPushButton('Dynamic Button', self)
+        self.dynamic_button.clicked.connect(self.dynamic_button_clicked)
+
         self.setLayout(QVBoxLayout())
         self.layout().addWidget(self.progress_label)
         self.layout().addWidget(self.close_button)
+        self.layout().addWidget(self.dynamic_button)
+        self.ocr_in_progress = True
 
     def close_window(self):
         self.close()
-        self.parent.show()
+        parent = self.parent
+        parent.show()
+
+    def dynamic_button_clicked(self):
+        self.progress_label.setText('Dynamic Button Clicked!')
 
 class LanguageSelector(QWidget):
     def __init__(self):
@@ -29,16 +37,21 @@ class LanguageSelector(QWidget):
         self.start_ocr_button = QPushButton('Start OCR', self)
         self.start_ocr_button.clicked.connect(self.start_ocr)
 
-        self.setLayout(QVBoxLayout())
-        self.layout().addWidget(self.start_ocr_button)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.start_ocr_button)
+
+        self.word_label = QLabel('Hello', self)
+        self.layout.addWidget(self.word_label)
+
+        self.setLayout(self.layout)
+
+        self.progress_window = None
 
     def start_ocr(self):
-        # Hide the main window
         self.hide()
-
-        # Open the OCR progress window
         self.progress_window = OCRProgressWindow(self)
         self.progress_window.show()
+        self.progress_window.ocr_in_progress = True
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
