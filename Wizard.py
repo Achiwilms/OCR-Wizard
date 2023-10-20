e6b97535b34c7dcf67af52a7cbd130ea74caf123
Achiwilms <philip901203777@gmail.com>
2023-10-20 20:52:37 +0800

    complete loop

--- Wizard.py
+++ Wizard.py
@@ -3,11 +3,12 @@ import ocrmypdf
 from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QPushButton, QApplication
 
 class Wizard(QWidget):
-    def __init__(self, LanguageSelector=None):
+    def __init__(self, LanguageSelector=None, FileSelector=None):
         super().__init__()    
         
-        # Set the language selector
+        # Set the selector
         self.LanguageSelector = LanguageSelector
+        self.FileSelector = FileSelector
 
         # Set the window title
         self.setWindowTitle('OCR Wizard')
@@ -17,12 +18,14 @@ class Wizard(QWidget):
         # Create a button to select file
         self.word_label = QLabel('', self)
         self.close_button = QPushButton('Close', self)
+        self.restart_button = QPushButton('OCR other files', self)
 
         # Create a layout to add checkboxes and the buttons
         layout = QVBoxLayout()
 
         # Add the button to the layout
         layout.addWidget(self.word_label)
+        layout.addWidget(self.restart_button)
         layout.addWidget(self.close_button)
 
         # Set the layout for the main window
@@ -30,6 +33,7 @@ class Wizard(QWidget):
 
         # Connect the buttons to their respective functions
         self.close_button.clicked.connect(self.close_window)
+        self.restart_button.clicked.connect(self.restart)
 
     # This function is called when the window is shown
     def showEvent(self, event):
@@ -39,7 +43,7 @@ class Wizard(QWidget):
 
     def ocr_process(self):
         # ocr all pdf files
-        for file_path in self.LanguageSelector.FileSelector.file_paths:
+        for file_path in self.FileSelector.file_paths:
             dir, filename = os.path.split(file_path)
             output_path = os.path.join(dir, f"OCR_{filename}")
             ocrmypdf.ocr(file_path, output_path, language=self.LanguageSelector.selected_languages)
@@ -50,6 +54,14 @@ class Wizard(QWidget):
         
     def close_window(self):
         self.close()
+
+    def restart(self):
+        # close this window
+        self.close()
+
+        # Open the language selector window and pass a reference to self
+        self.progress_window = self.FileSelector
+        self.progress_window.show()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
